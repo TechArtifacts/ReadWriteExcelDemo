6cd42e02908c880669a09af8dc179eb890d0ac16
BHUSHAN ARUN KADAM <bhushan.kadam2709@gmail.com>
2023-01-13 13:02:19 +0530

    Initialized Code

---
+++ ExcelHelper.cs
@@ -0,0 +1,61 @@
+﻿using OfficeOpenXml;
+
+namespace ReadWriteExcelDemo
+{
+    public class ExcelHelper
+    {
+        public ExcelHelper()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        public IList<Employee> ReadEmployeeDataFromExcel(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            var employees = new List<Employee>();
+
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                //get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
+
+                //We will start from row 2 since row 1 is a header row.
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    employees.Add(new Employee()
+                    {
+                        Id = Convert.ToInt32(worksheet.Cells[$"A{row}"].Value),
+                        FirstName = worksheet.Cells[$"B{row}"].Value?.ToString(),
+                        LastName = worksheet.Cells[$"C{row}"].Value?.ToString(),
+                        MobileNumber = worksheet.Cells[$"D{row}"].Value?.ToString()
+                    });
+                }
+            }
+            return employees;
+        }
+
+        public void AddEmployeeData(string filePath, Employee employee)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                //get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count                
+
+                //Add new data to the excel row
+                worksheet.Cells[$"A{rowCount + 1}"].Value = employee.Id;
+                worksheet.Cells[$"B{rowCount + 1}"].Value = employee.FirstName;
+                worksheet.Cells[$"C{rowCount + 1}"].Value = employee.LastName;
+                worksheet.Cells[$"D{rowCount + 1}"].Value = employee.MobileNumber;
+
+                package.Save();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using ReadWriteExcelDemo;
+
+var excelHelper = new ExcelHelper();
+
+Console.WriteLine("Writing new employee");
+//Add new employee
+excelHelper.AddEmployeeData("C:\\TestData\\Employees.xlsx", new Employee()
+{
+    Id = 5,
+    FirstName = "Suresh",
+    LastName = "Raina",
+    MobileNumber = "91-1111111115"
+});
+
+Console.WriteLine("Employee Data Saved Successfully!");
+
+Console.WriteLine();
+
+Console.WriteLine("Reading Employee Data");
+var employees = excelHelper.ReadEmployeeDataFromExcel("C:\\TestData\\Employees.xlsx");
+
+foreach (var employee in employees)
+{
+    Console.WriteLine($"Id: {employee.Id}, FirstName: {employee.FirstName}, LastName: {employee.LastName}, Mobile: {employee.MobileNumber}");
+}
+
+Console.Read();
+
+
